851bd47eefc2e1113d5d6ebb888312b9bed9d1f1
Alexandra Dache <alexandra.dache@student.umons.ac.be>
2022-12-17 20:15:59 +0100

    main nouvelle versions
    dans le main:
    correction et bonne version de grasp calcul cout ring , assignement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from utils import *
-from recherche_tabou import recherche_tabou
-from grasp import grasp1
-# Initialisation ----------------
+from random import randrange
+import time
+import numpy as np
+
 
 def read_instances(fileName):
     with open(fileName,'r') as fin :
@@ -17,38 +17,139 @@ def read_instances(fileName):
             cost_star.append([int(a) for a in fin.readline().rstrip().split(" ")])
     return cost_ring, cost_star
 
-# Solution initiale  --------------------------
+def assignement(star, cost_star, ring):
+
+    star_matrice = []
+    cout_star=0
+    for i in star:
+        # 1 fait toujours parti du ring
+        min = cost_star[i-1][0]
+        star_chemin = [i-1, 1]
+        for j in ring:
+            if cost_star[i-1][j-1] < min:
+                min = cost_star[i-1][j-1]
+                star_chemin = [i, j]
+        star_matrice.append(star_chemin)
+        cout_star+=min
+
+    return star_matrice,cout_star
+
+def coutduring(ring,cost_ring):
+    cout_ring=0
+    for i in range(len(ring)-1):
+        cout_ring += cost_ring[ring[i]-1][ring[i + 1]-1]
+    cout_ring += cost_ring[ring[len(ring)-1]-1][0]
+
+    return cout_ring
+
+def grasp1(cost_ring, cost_star, times):
+    start = time.perf_counter()
+    N = len(cost_ring)
+    best_ring = []
+    best_star = []
+    best_value = float("inf")
+    bestlist = []
+    best_alpha = 0
+    alphabest = []
+    t=0
+
+    while t < times:
+
+        for alpha in np.arange(0, 0.1s, 0.01):
+            print('alpha=', alpha)
+            ring = [1]
+            star = []
+            possible = [i for i in range(1, N+1)]
+            p = 1
+
+            while len(ring) + len(star) < N:
+                maximum = 0
+                minimum = float("inf")
+
+                for i in possible:
+
+                    if cost_ring[p][i-1] > maximum:
+                        maximum = cost_ring[p-1][i-1]
+
+                    if cost_star[p][i-1] > maximum:
+                        maximum = cost_star[p-1][i-1]
+
+                    if cost_ring[p][i-1] < minimum:
+                        minimum = cost_ring[p-1][i-1]
+
+                    if cost_star[p][i-1] < minimum:
+                        minimum = cost_star[p-1][i-1]
+
+                borne = minimum + (maximum - minimum) * alpha
+
+                options = []
+                itt = -1
+
+                for i in possible:
+                    if cost_ring[p-1][i-1] <= borne:
+                        options.append(i)
+                        itt += 1
+
+                for i in possible:
+                    if cost_star[p-1][i-1] <= borne:
+                        options.append(i)
+
+                if len(options) > 1:
+                    r = randrange(len(options))
+                else:
+                    r = 0
+
+                for j, i in enumerate(possible):
+                    if i == options[r]:
+                        del possible[j]
+
+                if r < itt:
+                    ring.append(options[r])
+                    p = options[r]
+                else:
+                    star.append(options[r])
+
+            star_mat,cout_star = assignement(star, cost_star, ring)
+            cout_ring=coutduring(ring, cost_ring)
+
+
+
+
+            if cout_star+cout_ring < best_value:
+                best_alpha = alpha
+                best_ring = ring
+                best_star = star
+                best_star_mat = star_mat
+                best_value =cout_star+cout_ring
+        print('time',t)
+        end = time.perf_counter()
+        t = end - start
+        alphabest.append(best_alpha)
+        print('meilleurs alpha=', alphabest)
+        bestlist.append(best_value)
+        print('list_best', bestlist)
+        print('best', min(bestlist))
+
+    return best_ring,best_star
+
+
+
+
+
+
+
+
+#######################################################################################
+
+
 fileName = 'instances/instance1.txt'
 cost_ring, cost_star = read_instances(fileName)
 
-# ring, star_matrice, star = grasp1(cost_ring, cost_star, 60)
+ring,star = grasp1(cost_ring, cost_star, 60)
 
-# verif(ring, star_matrice, star, len(cost_ring))
 
-ring, star_matrice, star = ini_min_somme(cost_ring, cost_star)
 
-best_ring, best_star_mat, best_star = recherche_tabou(cost_ring, cost_star, ring, star_matrice, star, len_list_tabou=10, nbr_voisinage=10, temps=15)
-# initialisation aleatoire
 
-#
-# # Algorithme --------------------------
-#
-# #ring, star_matrice, star = recuit_variable(cost_ring, cost_star, ring, star_matrice, star)
-# #[ring ,star_matrice,star]=recherche_tabou(cost_ring, cost_star, ring, star_matrice, star, 10);
-# #[ring ,star_matrice,star]=fourmis(cost_ring, cost_star, ring, star_matrice, star);
-#
-# # Verification de la solution  --------------------------
-#
-#verif(ring, star_matrice, star)
-#
-# # Calcul du Cost ----------------
-#
-# cout_total = cout(cost_ring, cost_star, ring, star_matrice)
-#
-# # Output ------------------------
-#
-# # faire la transition
-#
-# ##dlmwrite(['output/out',fileName],["RING ",mat2str(length(Listering))],delim="");
-# ##dlmwrite(['output/out',fileName],Listering,delim=" ", "-append");
-#
+
+
+
